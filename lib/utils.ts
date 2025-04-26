a025712450a01a2d5c016df9c69dbbd6d84dab26
Gunjan Tomer <gunjantomer92@gmail.com>
2025-04-26 18:58:00 -0400

    feat: add some util functions

--- lib/utils.ts
+++ lib/utils.ts
@@ -1,6 +1,13 @@
-import { clsx, type ClassValue } from "clsx"
-import { twMerge } from "tailwind-merge"
+import { clsx, type ClassValue } from 'clsx';
+import { twMerge } from 'tailwind-merge';
 
 export function cn(...inputs: ClassValue[]) {
-  return twMerge(clsx(inputs))
+  return twMerge(clsx(inputs));
 }
+
+export function joinClassNames(...classNames: MaybeString[]) {
+  return classNames.filter((c) => !!c).join(' ') || undefined;
+}
+
+/* eslint-disable @typescript-eslint/no-unused-vars */
+type MaybeString = string | false | null | undefined;
